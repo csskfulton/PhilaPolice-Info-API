b5152690696874413712be438a13b0978311a1ed
csskfulton <csskfulton@gmail.com>
2019-03-31 18:34:19 -0400

    CalendarObject Class Added

---
+++ CalendarObject.php
@@ -0,0 +1,33 @@
+<?php 
+
+        class CalendarObject{
+            
+                
+//            var $CalendarID;
+            var $TimeStamp;
+            var $DistrictNumber;
+            var $Title;
+            var $MeetDate;
+            var $MeetLocation;
+//             var $ScrapeHash;
+            
+//            function setCalendarID($CalendarID) { $this->CalendarID = $CalendarID; }
+//            function getCalendarID() { return $this->CalendarID; }
+            function setTimeStamp($TimeStamp) { $this->TimeStamp = $TimeStamp; }
+            function getTimeStamp() { return $this->TimeStamp; }
+            function setDistrictNumber($DistrictNumber) { $this->DistrictNumber = $DistrictNumber; }
+            function getDistrictNumber() { return $this->DistrictNumber; }
+            function setTitle($Title) { $this->Title = $Title; }
+            function getTitle() { return $this->Title; }
+            function setMeetDate($MeetDate) { $this->MeetDate = $MeetDate; }
+            function getMeetDate() { return $this->MeetDate; }
+            function setMeetLocation($MeetLocation) { $this->MeetLocation = $MeetLocation; }
+            function getMeetLocation() { return $this->MeetLocation; }
+//             function setScrapeHash($ScrapeHash) { $this->ScrapeHash = $ScrapeHash; }
+//             function getScrapeHash() { return $this->ScrapeHash; }
+            
+            
+            
+        }
+
+?>
