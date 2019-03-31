bbe338cdbfc11a31dc3f6ea8d7c9354dca971f49
csskfulton <csskfulton@gmail.com>
2019-03-31 18:30:33 -0400

    NewsObject Class vars updated

--- NewsObject.php
+++ NewsObject.php
@@ -1,118 +1,51 @@
 <?php
 
-    
     class NewsObject{
         
         
-       var $id;
-       var $timeStamp;
-       var $storyAuthor;
-       var $districtNumber;
-       var $title;
-       var $url;
-       var $description;
-       var $GUID;
-       var $pubDate;
-       var $category;
-       var $imageURL;
-       var $scrapeHash;
-       var $tubeURL;
-       
-       
-       function set_id($id) {
-           $this->id = $id;
-       }
-       
-       function get_id() {
-           return $this->id;
-       }
-       function set_timeStamp($timeStamp) {
-           $this->timeStamp = $timeStamp;
-       }
-       
-       function get_timeStamp() {
-           return $this->timeStamp;
-       }
-       function set_storyAuthor($storyAuthor) {
-           $this->storyAuthor = $storyAuthor;
-       }
-       
-       function get_storyAuthor() {
-           return $this->storyAuthor;
-       }
-       function set_districtNumber($districtNumber) {
-           $this->districtNumber = $districtNumber;
-       }
-       
-       function get_districtNumber() {
-           return $this->districtNumber;
-       }
-       function set_title($title) {
-           $this->title = $title;
-       }
-       
-       function get_title() {
-           return $this->title;
-       }
-       function set_url($url) {
-           $this->url = $url;
-       }
-       
-       function get_url() {
-           return $this->url;
-       }
-       function set_description($description) {
-           $this->description = $description;
-       }
-       
-       function get_description() {
-           return $this->description;
-       }
-       function set_GUID($GUID) {
-           $this->GUID = $GUID;
-       }
-       
-       function get_GUID() {
-           return $this->GUID;
-       }
-       function set_pubDate($pubDate) {
-           $this->pubDate = $pubDate;
-       }
-       
-       function get_pubDate() {
-           return $this->pubDate;
-       }
-       function set_category($category) {
-           $this->category = $category;
-       }
-       
-       function get_category() {
-           return $this->category;
-       }
-       function set_imageURL($imageURL) {
-           $this->imageURL = $imageURL;
-       }
-       
-       function get_imageURL() {
-           return $this->imageURL;
-       }
-       function set_scrapeHash($scrapeHash) {
-           $this->scrapeHash = $scrapeHash;
-       }
-       
-       function get_scrapeHash() {
-           return $this->scrapeHash;
-       }
-       
-       function set_tubeURL($tubeURL) {
-           $this->tubeURL = $tubeURL;
-       }
-       
-       function get_tubeURL() {
-           return $this->tubeURL;
-       }
+        var $NewsStoryID;
+        var $TimeStamp;
+        var $StoryAuthor;
+        var $DistrictNumber;
+        var $Title;
+        var $URL;
+        var $Description;
+        var $GUID;
+        var $PubDate;
+        var $Category;
+        var $ImageURL;
+        var $ScrapeHash;
+        var $TubeURL;
+        var $MasterHash;
         
-        
+        function setNewsStoryID($NewsStoryID) { $this->NewsStoryID = $NewsStoryID; }
+        function getNewsStoryID() { return $this->NewsStoryID; }
+        function setTimeStamp($TimeStamp) { $this->TimeStamp = $TimeStamp; }
+        function getTimeStamp() { return $this->TimeStamp; }
+        function setStoryAuthor($StoryAuthor) { $this->StoryAuthor = $StoryAuthor; }
+        function getStoryAuthor() { return $this->StoryAuthor; }
+        function setDistrictNumber($DistrictNumber) { $this->DistrictNumber = $DistrictNumber; }
+        function getDistrictNumber() { return $this->DistrictNumber; }
+        function setTitle($Title) { $this->Title = $Title; }
+        function getTitle() { return $this->Title; }
+        function setURL($URL) { $this->URL = $URL; }
+        function getURL() { return $this->URL; }
+        function setDescription($Description) { $this->Description = $Description; }
+        function getDescription() { return $this->Description; }
+        function setGUID($GUID) { $this->GUID = $GUID; }
+        function getGUID() { return $this->GUID; }
+        function setPubDate($PubDate) { $this->PubDate = $PubDate; }
+        function getPubDate() { return $this->PubDate; }
+        function setCategory($Category) { $this->Category = $Category; }
+        function getCategory() { return $this->Category; }
+        function setImageURL($ImageURL) { $this->ImageURL = $ImageURL; }
+        function getImageURL() { return $this->ImageURL; }
+        function setScrapeHash($ScrapeHash) { $this->ScrapeHash = $ScrapeHash; }
+        function getScrapeHash() { return $this->ScrapeHash; }
+        function setTubeURL($TubeURL) { $this->TubeURL = $TubeURL; }
+        function getTubeURL() { return $this->TubeURL; }
+        function setMasterHash($MasterHash) { $this->MasterHash = $MasterHash; }
+        function getMasterHash() { return $this->MasterHash; }
         
     }
 
