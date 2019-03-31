fab7122b5e5ab42b4c536de934d2cf37261fb537
csskfulton <csskfulton@gmail.com>
2019-03-31 18:28:45 -0400

    DistrictInfoObject Class Added

---
+++ DistrictInfoObject.php
@@ -0,0 +1,39 @@
+<?php 
+
+    class DistrictInfoObject{
+        
+//        var $DistrictInfoID;
+//        var $TimeStamp;
+        var $DistrictNumber;
+        var $LocationAddress;
+        var $PhoneNumber;
+        var $EmailAddress;
+        var $CaptainName;
+        var $CaptainImageURL;
+        var $DetectiveDivision;
+        
+//        function setDistrictInfoID($DistrictInfoID) { $this->DistrictInfoID = $DistrictInfoID; }
+//        function getDistrictInfoID() { return $this->DistrictInfoID; }
+//         function setTimeStamp($TimeStamp) { $this->TimeStamp = $TimeStamp; }
+//         function getTimeStamp() { return $this->TimeStamp; }
+        function setDistrictNumber($DistrictNumber) { $this->DistrictNumber = $DistrictNumber; }
+        function getDistrictNumber() { return $this->DistrictNumber; }
+        function setLocationAddress($LocationAddress) { $this->LocationAddress = $LocationAddress; }
+        function getLocationAddress() { return $this->LocationAddress; }
+        function setPhoneNumber($PhoneNumber) { $this->PhoneNumber = $PhoneNumber; }
+        function getPhoneNumber() { return $this->PhoneNumber; }
+        function setEmailAddress($EmailAddress) { $this->EmailAddress = $EmailAddress; }
+        function getEmailAddress() { return $this->EmailAddress; }
+        function setCaptainName($CaptainName) { $this->CaptainName = $CaptainName; }
+        function getCaptainName() { return $this->CaptainName; }
+        function setCaptainImageURL($CaptainImageURL) { $this->CaptainImageURL = $CaptainImageURL; }
+        function getCaptainImageURL() { return $this->CaptainImageURL; }
+        function setDetectiveDivision($DetectiveDivision) { $this->DetectiveDivision = $DetectiveDivision; }
+        function getDetectiveDivision() { return $this->DetectiveDivision; }
+        
+        
+        
+    }
+    
+
+?>
