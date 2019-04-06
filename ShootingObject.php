aa98f2d8699b5df5e190c0da4b18d8e8e841d9ab
csskfulton <csskfulton@gmail.com>
2019-04-06 11:23:36 -0400

    ShootingObject Added

---
+++ ShootingObject.php
@@ -0,0 +1,76 @@
+<?php 
+
+class ShootingObject{
+    
+
+var $ShootingID;
+var $TimeStamp;
+var $DistrictNumber;
+var $Year;
+var $CrimeTime;
+var $DCNumber;
+var $CrimeDate;
+var $Race;
+var $Gender;
+var $Age;
+var $Wound;
+var $isOfficerInvolved;
+var $isOffenderInjured;
+var $isOffenderDeceased;
+var $LocationAddress;
+var $LocationX;
+var $LocationY;
+var $isInside;
+var $isOutside;
+var $isFatal;
+
+function setShootingID($ShootingID) { $this->ShootingID = $ShootingID; }
+function getShootingID() { return $this->ShootingID; }
+function setTimeStamp($TimeStamp) { $this->TimeStamp = $TimeStamp; }
+function getTimeStamp() { return $this->TimeStamp; }
+function setDistrictNumber($DistrictNumber) { $this->DistrictNumber = $DistrictNumber; }
+function getDistrictNumber() { return $this->DistrictNumber; }
+function setYear($Year) { $this->Year = $Year; }
+function getYear() { return $this->Year; }
+function setCrimeTime($CrimeTime) { $this->CrimeTime = $CrimeTime; }
+function getCrimeTime() { return $this->CrimeTime; }
+function setDCNumber($DCNumber) { $this->DCNumber = $DCNumber; }
+function getDCNumber() { return $this->DCNumber; }
+function setCrimeDate($CrimeDate) { $this->CrimeDate = $CrimeDate; }
+function getCrimeDate() { return $this->CrimeDate; }
+function setRace($Race) { $this->Race = $Race; }
+function getRace() { return $this->Race; }
+function setGender($Gender) { $this->Gender = $Gender; }
+function getGender() { return $this->Gender; }
+function setAge($Age) { $this->Age = $Age; }
+function getAge() { return $this->Age; }
+function setWound($Wound) { $this->Wound = $Wound; }
+function getWound() { return $this->Wound; }
+function setIsOfficerInvolved($isOfficerInvolved) { $this->isOfficerInvolved = $isOfficerInvolved; }
+function getIsOfficerInvolved() { return $this->isOfficerInvolved; }
+function setIsOffenderInjured($isOffenderInjured) { $this->isOffenderInjured = $isOffenderInjured; }
+function getIsOffenderInjured() { return $this->isOffenderInjured; }
+function setIsOffenderDeceased($isOffenderDeceased) { $this->isOffenderDeceased = $isOffenderDeceased; }
+function getIsOffenderDeceased() { return $this->isOffenderDeceased; }
+function setLocationAddress($LocationAddress) { $this->LocationAddress = $LocationAddress; }
+function getLocationAddress() { return $this->LocationAddress; }
+function setLocationX($LocationX) { $this->LocationX = $LocationX; }
+function getLocationX() { return $this->LocationX; }
+function setLocationY($LocationY) { $this->LocationY = $LocationY; }
+function getLocationY() { return $this->LocationY; }
+function setIsInside($isInside) { $this->isInside = $isInside; }
+function getIsInside() { return $this->isInside; }
+function setIsOutside($isOutside) { $this->isOutside = $isOutside; }
+function getIsOutside() { return $this->isOutside; }
+function setIsFatal($isFatal) { $this->isFatal = $isFatal; }
+function getIsFatal() { return $this->isFatal; }
+
+
+
+}
+
+
+
+
+
+?>
