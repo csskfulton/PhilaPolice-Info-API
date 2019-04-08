237291052941b32df2f427cac01e79d1b84244e3
csskfulton <csskfulton@gmail.com>
2019-04-08 12:52:13 -0400

    UCVideoObject Added

---
+++ UCVideoObject.php
@@ -0,0 +1,67 @@
+<?php 
+
+
+
+    
+    class UCVideoObject{
+        
+        var $UCVideoID;
+        var $NewsStoryID;
+        var $TimeStamp;
+        var $VideoTitle;
+        var $Description;
+        var $VideoID;
+        var $VideoImageURL;
+        var $VideoDate;
+        var $DistrictNumber;
+        var $CrimeType;
+        var $TubeURL;
+        var $DistrictDivision;
+        var $HashTag;
+        
+        
+        function setUCVideoID($UCVideoID) { $this->UCVideoID = $UCVideoID; }
+        function getUCVideoID() { return $this->UCVideoID; }
+        function setNewsStoryID($NewsStoryID) { $this->NewsStoryID = $NewsStoryID; }
+        function getNewsStoryID() { return $this->NewsStoryID; }
+        function setTimeStamp($TimeStamp) { $this->TimeStamp = $TimeStamp; }
+        function getTimeStamp() { return $this->TimeStamp; }
+        function setVideoTitle($VideoTitle) { $this->VideoTitle = $VideoTitle; }
+        function getVideoTitle() { return $this->VideoTitle; }
+        function setDescription($Description) { $this->Description = $Description; }
+        function getDescription() { return $this->Description; }
+        function setVideoID($VideoID) { $this->VideoID = $VideoID; }
+        function getVideoID() { return $this->VideoID; }
+        function setVideoImageURL($VideoImageURL) { $this->VideoImageURL = $VideoImageURL; }
+        function getVideoImageURL() { return $this->VideoImageURL; }
+        function setVideoDate($VideoDate) { $this->VideoDate = $VideoDate; }
+        function setDistrictNumber($DistrictNumber) { $this->DistrictNumber = $DistrictNumber; }
+        function getDistrictNumber() { return $this->DistrictNumber; }
+        function getVideoDate() { return $this->VideoDate; }
+        function setCrimeType($CrimeType) { $this->CrimeType = $CrimeType; }
+        function getCrimeType() { return $this->CrimeType; }
+        function setTubeURL($TubeURL) { $this->TubeURL = $TubeURL; }
+        function getTubeURL() { return $this->TubeURL; }
+        function setDistrictDivision($DistrictDivision) { $this->DistrictDivision = $DistrictDivision; }
+        function getDistrictDivision() { return $this->DistrictDivision; }
+        function setHashTag($HashTag) { $this->HashTag = $HashTag; }
+        function getHashTag() { return $this->HashTag; }
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
